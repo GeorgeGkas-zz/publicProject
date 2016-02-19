1f899d2a1aa1de09edea47d25f8770e563db8c0e
GeorgeGkas <GeorgeGkas@users.noreply.github.com>
2016-02-19 21:53:05 +0200

    Update main.class.php

--- UserAuth/main.class.php
+++ UserAuth/main.class.php
@@ -1,55 +1,55 @@
 <?php 
-	require_once '../lib/config.php';
+    require_once '../lib/config.php';
 
-	/**
-	* 
-	*/
-	class Main
-	{
-		protected $DHB	=	NULL;
+    /**
+    * 
+    */
+    class Main
+    {
+        protected $DHB  =   NULL;
 
-		function __construct()
-		{
-			try {
-		        # DHB : Database Handle
-		        $this->DHB = new PDO("mysql:host=".DB_HOST.";dbname=".DB_DATABSE, DB_USER, DB_PASSWORD);
-		        $this->DHB->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-		    }
-		    catch(PDOException $e) {
-		        file_put_contents('../PDOErrors.txt', $e->getMessage(), FILE_APPEND);
-		    }
-		}
+        function __construct()
+        {
+            try {
+                # DHB : Database Handle
+                $this->DHB = new PDO("mysql:host=".DB_HOST.";dbname=".DB_DATABSE, DB_USER, DB_PASSWORD);
+                $this->DHB->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+            }
+            catch(PDOException $e) {
+                file_put_contents('../PDOErrors.txt', $e->getMessage(), FILE_APPEND);
+            }
+        }
 
-		/*public function dbConn(PDO $dbConn) {
-			$this->DHB = $dbConn;
-		}*/
+        /*public function dbConn(PDO $dbConn) {
+            $this->DHB = $dbConn;
+        }*/
 
-		protected function UserIsLogin() {
-			if (isset($_SESSION['login'])) {
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
+        protected function UserIsLogin() {
+            if (isset($_SESSION['login'])) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
 
-		protected function getUserEmail() {
-			return $_SESSION['login'];
-		}
+        protected function getUserEmail() {
+            return $_SESSION['login'];
+        }
 
-		protected function setUserEmail($email) {
-			$_SESSION['login'] = $email;
-		}
+        protected function setUserEmail($email) {
+            $_SESSION['login'] = $email;
+        }
 
-		protected function clearUserEmail() {
-			unset($_SESSION['login']); 
-		}
+        protected function clearUserEmail() {
+            unset($_SESSION['login']); 
+        }
 
-		protected function setReturnState($msg, $state = false) {
-			return array('State' => $state, 'Msg' => $msg);
-		}
+        protected function setReturnState($msg, $state = false) {
+            return array('State' => $state, 'Msg' => $msg);
+        }
 
-	}
+    }
 
 
 ?>
